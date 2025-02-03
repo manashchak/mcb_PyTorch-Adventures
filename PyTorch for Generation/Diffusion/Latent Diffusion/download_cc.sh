c669a69fa5a7deef0253b6947645a0dc67d86dfb
priyammaz <priyammazumdar1@gmail.com>
2025-02-03 06:32:06 -0600

    pushing up data download scripts

--- PyTorch for Generation/Diffusion/Latent Diffusion/download_cc.sh
+++ PyTorch for Generation/Diffusion/Latent Diffusion/download_cc.sh
@@ -4,4 +4,6 @@
 python prep_cc_dataset.py \
     --path_to_root /mnt/datadrive/data/ConceptualCaptions \
     --path_to_store /mnt/datadrive/data/ConceptualCaptions \
-    --num_workers 32
+    --num_workers 32 \
+    --batch_size 100 \
+    --sample_pct 0.2
--- PyTorch for Generation/Diffusion/Latent Diffusion/prep_cc_dataset.py
+++ PyTorch for Generation/Diffusion/Latent Diffusion/prep_cc_dataset.py
@@ -28,14 +28,23 @@ def download_image(example):
 
     return example 
 
-def build_hf_dataset(path_to_tsv, path_to_store, num_workers=32):
+def build_hf_dataset(path_to_train_tsv, path_to_val_tsv, path_to_store, sample_pct=1.0, num_workers=32):
 
     dataset = load_dataset("csv", 
-                           data_files=path_to_tsv, 
+                           data_files={"train": path_to_train_tsv, "test": path_to_val_tsv}, 
                            delimiter='\t',
                            column_names=["text", "img_link"], 
                            header=None,
                            cache_dir=path_to_store)
+
+    if sample_pct != 1.0:
+        num_train_samples = len(dataset["train"])
+        samples_to_keep = int(num_train_samples * sample_pct)
+        
+        print(f"Keeping {sample_pct*100}% of the Training Data: {samples_to_keep}/{num_train_samples}")
+        dataset["train"] = dataset["train"].shuffle().select(range(samples_to_keep))
+    
+    print(dataset)
     
     dataset = dataset.map(download_image, batched=True, batch_size=100, num_proc=num_workers)
     dataset = dataset.filter(lambda x: x["image"] is not None, num_proc=num_workers)
@@ -79,22 +88,29 @@ if __name__ == "__main__":
                         required=False, 
                         type=str)
     
+    parser.add_argument("--batch_size",
+                        help="Number of samples to download per thread",
+                        default=100, 
+                        required=False, 
+                        type=int)
+    
     parser.add_argument("--num_workers", 
                         help="Path to Train and Validation tsv downloads",
                         default=32,
+                        required=False,
                         type=int)
     
     args = parser.parse_args()
 
-    build_hf_dataset(path_to_tsv=os.path.join(args.path_to_root, args.val_tsv_name),
+    build_hf_dataset(path_to_train_tsv=os.path.join(args.path_to_root, args.train_tsv_name),
+                     path_to_val_tsv=os.path.join(args.path_to_root, args.val_tsv_name),
                      path_to_store=args.path_to_store,
+                     sample_pct=args.sample_pct,
                      num_workers=args.num_workers)
 
     from datasets import load_from_disk
     dataset = load_from_disk(args.path_to_store)
     
-    idx = 125
-    dataset["train"][idx]["image"].show()
-    print(dataset["train"][idx]["text"])
+    print(dataset)
         
     
