c59bd383137f764f3feb19fb1fffd552ec8b015e
priyammaz <priyammazumdar1@gmail.com>
2025-02-02 19:51:10 -0600

    pushing up start to dataset prep

---
+++ PyTorch for Generation/Diffusion/Latent Diffusion/download_cc.sh
@@ -0,0 +1,7 @@
+### Download Conceptual Captions Images from TSVs 
+### You can get the TSV files @ https://ai.google.com/research/ConceptualCaptions/download
+
+python prep_cc_dataset.py \
+    --path_to_root /mnt/datadrive/data/ConceptualCaptions \
+    --path_to_store /mnt/datadrive/data/ConceptualCaptions \
+    --num_workers 32
--- PyTorch for Generation/Diffusion/Latent Diffusion/prep_cc_dataset.py
+++ PyTorch for Generation/Diffusion/Latent Diffusion/prep_cc_dataset.py
@@ -1,40 +1,100 @@
-from concurrent.futures import ThreadPoolExecutor
-from functools import partial
-import io
-import urllib
-import PIL.Image
+import os
 from datasets import load_dataset
-from datasets.utils.file_utils import get_datasets_user_agent
+from io import BytesIO
+from PIL import Image
+import requests
+import argparse
 
 
-USER_AGENT = get_datasets_user_agent()
+def download_image(example):
 
+    images = []
 
-def fetch_single_image(image_url, timeout=None, retries=0):
-    for _ in range(retries + 1):
+    for img_link in example["img_link"]:
         try:
-            request = urllib.request.Request(
-                image_url,
-                data=None,
-                headers={"user-agent": USER_AGENT},
-            )
-            with urllib.request.urlopen(request, timeout=timeout) as req:
-                image = PIL.Image.open(io.BytesIO(req.read()))
-            break
-        except Exception:
+
+            response = requests.get(img_link, timeout=10)
+            response.raise_for_status()
+
+            image = Image.open(BytesIO(response.content))
+
+        except:
+            
             image = None
-    return image
 
+        images.append(image)
 
-def fetch_images(batch, num_threads, timeout=None, retries=0):
-    fetch_single_image_with_args = partial(fetch_single_image, timeout=timeout, retries=retries)
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        batch["image"] = list(executor.map(fetch_single_image_with_args, batch["image_url"]))
-    return batch
+    example["image"] = images
+
+    return example 
+
+def build_hf_dataset(path_to_tsv, path_to_store, num_workers=32):
+
+    dataset = load_dataset("csv", 
+                           data_files=path_to_tsv, 
+                           delimiter='\t',
+                           column_names=["text", "img_link"], 
+                           header=None,
+                           cache_dir=path_to_store)
+    
+    dataset = dataset.map(download_image, batched=True, batch_size=100, num_proc=num_workers)
+    dataset = dataset.filter(lambda x: x["image"] is not None, num_proc=num_workers)
+    
+    dataset.save_to_disk(path_to_store)
+
+    
+    
+
 
 if __name__ == "__main__":
-    num_threads = 16
-    dset = load_dataset("google-research-datasets/conceptual_captions", cache_dir="/mnt/datadrive/data/ConceptualCaptions")
-    dset = dset.map(fetch_images, fn_kwargs={"num_threads": num_threads})
-    print(dset)
+
+    parser = argparse.ArgumentParser(description="Download and Prepare ConceptualCaption-3M")
+    
+    parser.add_argument("--path_to_root", 
+                        help="Path to Train and Validation tsv downloads",
+                        required=True,
+                        type=str)
+    
+    parser.add_argument("--path_to_store",
+                        help="Will create a train/validation folder at this directory",
+                        required=True,
+                        type=str)
+    
+    parser.add_argument("--sample_pct",
+                        help="If you don't want all 3 Million images, random sample some proportion",
+                        default=1.0, 
+                        required=False, 
+                        type=float)
+                        
+    
+    parser.add_argument("--train_tsv_name",
+                        help="Name of the training tsv file",
+                        default="train.tsv", 
+                        required=False, 
+                        type=str)
+    
+    parser.add_argument("--val_tsv_name",
+                        help="Name of the validation tsv file",
+                        default="validation.tsv", 
+                        required=False, 
+                        type=str)
+    
+    parser.add_argument("--num_workers", 
+                        help="Path to Train and Validation tsv downloads",
+                        default=32,
+                        type=int)
+    
+    args = parser.parse_args()
+
+    build_hf_dataset(path_to_tsv=os.path.join(args.path_to_root, args.val_tsv_name),
+                     path_to_store=args.path_to_store,
+                     num_workers=args.num_workers)
+
+    from datasets import load_from_disk
+    dataset = load_from_disk(args.path_to_store)
+    
+    idx = 125
+    dataset["train"][idx]["image"].show()
+    print(dataset["train"][idx]["text"])
+        
     
