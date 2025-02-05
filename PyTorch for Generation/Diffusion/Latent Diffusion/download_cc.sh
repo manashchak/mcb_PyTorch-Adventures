7afee8d2bbe5ac6994ec595ea85de34f27f58bb5
priyammaz <priyammazumdar1@gmail.com>
2025-02-05 10:51:13 -0600

    using img2dataset instead to download

--- PyTorch for Generation/Diffusion/Latent Diffusion/download_cc.sh
+++ PyTorch for Generation/Diffusion/Latent Diffusion/download_cc.sh
@@ -1,9 +1,17 @@
 ### Download Conceptual Captions Images from TSVs 
 ### You can get the TSV files @ https://ai.google.com/research/ConceptualCaptions/download
 
-python prep_cc_dataset.py \
-    --path_to_root /mnt/datadrive/data/ConceptualCaptions \
-    --path_to_store /mnt/datadrive/data/ConceptualCaptions \
-    --num_workers 32 \
-    --batch_size 100 \
-    --sample_pct 0.25
+### Use img2dataset To Download Images ###
+### https://github.com/rom1504/img2dataset
+img2dataset --url_list "/mnt/datadrive/data/ConceptualCaptions/cc3m.tsv" \
+            --input_format "tsv"\
+            --url_col "url" \
+            --caption_col "caption" \
+            --output_format parquet \
+            --output_folder /mnt/datadrive/data/ConceptualCaptions/train \
+            --processes_count 1 \
+            --thread_count 48 \
+            --image_size 256 \
+            --resize_mode keep_ratio \
+            --enable_wandb True \
+            --wandb_project "DownloadCC3MLocal"
