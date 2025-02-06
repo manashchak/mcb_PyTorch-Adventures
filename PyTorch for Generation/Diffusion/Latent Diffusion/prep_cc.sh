8046f5ceeb041d772500ac31c89c4b1021d6c923
priyammaz <priyammazumdar1@gmail.com>
2025-02-06 11:48:35 -0600

    example cc3m data script prep

---
+++ PyTorch for Generation/Diffusion/Latent Diffusion/prep_cc.sh
@@ -0,0 +1,8 @@
+python prep_cc.py --path_to_data_root /mnt/datadrive/data/ConceptualCaptions/train \
+                  --path_to_save /mnt/datadrive/data/ConceptualCaptions/hf_train \
+                  --hf_clip_model_name openai/clip-vit-large-patch14 \
+                  --hf_cache_dir /mnt/datadrive/data/ConceptualCaptions/hf_cache \
+                  --cpu_batch_size 512 \
+                  --gpu_batch_size 256 \
+                  --num_cpu_workers 32 \
+                  --dtype bfloat16
