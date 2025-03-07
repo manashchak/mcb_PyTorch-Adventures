5f61697332d3f8854640cf2024e960946887deba
priyammaz <priyammazumdar1@gmail.com>
2025-03-07 11:45:45 -0600

    added in conceptual captions collation

--- PyTorch for Generation/Diffusion/Latent Diffusion/dataset.py
+++ PyTorch for Generation/Diffusion/Latent Diffusion/dataset.py
@@ -1,6 +1,4 @@
 import os
-import json
-import random
 from PIL import Image
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -54,17 +52,30 @@ class GenericImageDataset(Dataset):
     Args:
         - path_to_data: Points to a folder full of images of faces 
         - nested: Does that path_to_data contain folders, in which there are images
+        - return_classes: Do you want to return the class label (only available when nested=True)
     
     """
 
-    def __init__(self, path_to_data, nested=False, transform=None):
+    def __init__(self, 
+                 path_to_data, 
+                 nested=False, 
+                 transform=None,
+                 return_classes=True):
+        
         self.transforms = transform
+        self.return_classes = return_classes
 
         if not nested:
+            if return_classes:
+                raise Exception("Dataset is not nested, there are no class labels!")
             self.path_to_files = [os.path.join(path_to_data, file) for file in os.listdir(path_to_data)]
+            self.classes = None
         else:
             self.path_to_files = []
-            for dir in os.listdir(path_to_data):
+            classes = os.listdir(path_to_data)
+            self.classes = {cls:idx for (idx, cls) in enumerate(classes)}
+
+            for dir in classes:
                 path_to_dir = os.path.join(path_to_data, dir)
                 self.path_to_files.extend([os.path.join(path_to_dir, file) for file in os.listdir(path_to_dir)])
     
@@ -73,16 +84,25 @@ class GenericImageDataset(Dataset):
     
     def __getitem__(self, idx):
         img_path = self.path_to_files[idx]
+
+        if self.classes is not None:
+            class_label = self.classes[img_path.split("/")[-2]]
+
         img = Image.open(img_path)
         img = self.transforms(img)
+        
+        if self.return_classes:
+            return {"images": img,
+                    "class_conditioning": class_label}
+        else:
+            return {"images": img}
+        
 
-        return {"images": img}    
-
-def conceptual_captions(path_to_data, transforms, return_caption):
+def conceptual_captions(path_to_data, transforms):
 
     dataset = load_from_disk(path_to_data)
 
-    def img_transforms(batch):
+    def sample_transforms(batch):
 
         transformed_images = [
             transforms(image) for image in batch["image"]
@@ -94,15 +114,53 @@ def conceptual_captions(path_to_data, transforms, return_caption):
 
         return batch
 
-    dataset.set_transform(img_transforms)
-
-    if not return_caption:
-        if "encoded_text" in dataset.column_names["train"]:
-            dataset = dataset.remove_columns("encoded_text")
-        if "caption" in dataset.column_names["train"]:
-            dataset = dataset.remove_columns("caption")
+    dataset.set_transform(sample_transforms)
 
     return dataset["train"]
+
+def ConceptualCaptionsCollateFunction(model_name="openai/clip-vit-large-patch14",
+                                      pre_encoded_text=True, 
+                                      return_captions=True):
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    def _collate_fn(batch):
+
+        images = [b["images"] for b in batch]
+
+        if return_captions:
+            if pre_encoded_text:
+
+                if "encoded_text" not in batch[0].keys():
+                    raise Exception("Conceptual Captions is not pre-encoded, use pre_encoded_text=False")
+                
+                seq_lens = [len(b["encoded_text"]) for b in batch]
+                text_conditioning = [torch.tensor(b["encoded_text"]) for b in batch]
+                text_conditioning = torch.nn.utils.rnn.pad_sequence(text_conditioning,
+                                                                    batch_first=True)
+                
+            else:
+
+                if "caption" not in batch[0].keys():
+                    raise Exception("Conceptual Captions is pre-encoded, use pre_encoded_text=True")
+                
+                seq_lens = [len(b["caption"]) for b in batch]
+                text_conditioning = [torch.tensor(tokenizer.encode(b["caption"])) for b in batch] 
+                text_conditioning = torch.nn.utils.rnn.pad_sequence(text_conditioning,
+                                                                    batch_first=True,
+                                                                    padding_value=tokenizer.pad_token_id)
+                
+            attention_mask = torch.nn.utils.rnn.pad_sequence([torch.ones(s) for s in seq_lens],
+                                                            batch_first=True, 
+                                                            padding_value=0).bool()
+            return {"images": images, 
+                    "text_conditioning": text_conditioning, 
+                    "text_attention_mask": attention_mask}
+        
+        else:
+            return {"images": images}
+
+    return _collate_fn
 
 def get_dataset(dataset,
                 path_to_data, 
@@ -112,7 +170,10 @@ def get_dataset(dataset,
                 interpolation="bilinear",
                 random_flip_p=0.5,
                 train=True,
-                return_caption=True):
+                return_caption=True, 
+                return_classes=True,
+                text_encoder_model="openai/clip-vit-large-patch14",
+                pre_encoded_text=True):
     
     img_transform = image_transforms(num_channels=num_channels,
                                      img_size=img_size, 
@@ -120,73 +181,58 @@ def get_dataset(dataset,
                                      interpolation=interpolation, 
                                      random_flip_p=random_flip_p, 
                                      train=train)
-    if dataset == "celeba":
 
-        if return_caption:
-            raise Exception("CelebA Has No Captions!")
-        
-        trainset = GenericImageDataset(path_to_data=path_to_data, 
-                                       transform=img_transform)
-         
-    elif dataset == "celebahq":
+    if dataset == "celebahq":
 
         if return_caption:
             raise Exception("CelebAHQ Has No Captions!")
-        
-        trainset = GenericImageDataset(path_to_data=path_to_data, 
-                                       transform=img_transform)
-        
-    elif dataset == "ffhd":
-    
-        if return_caption:
-            raise Exception("FFHQ Has No Caption")
+        if return_classes:
+            raise Exception("celeba Has no Classes!")
         
         trainset = GenericImageDataset(path_to_data=path_to_data, 
                                        transform=img_transform,
-                                       nested=True)
+                                       nested=False, 
+                                       return_classes=False)
+        
+        collate_fn = None
         
     elif dataset == "imagenet": 
         if return_caption:
             raise Exception("Imagenet Has No Captions!")
 
-        trainset = GenericImageDataset(path_to_data=path_to_data, 
-                                       transform=img_transform, 
-                                       nested=True)
-        
-    elif dataset == "birds":
-        if return_caption:
-            raise Exception("CUB Birds has no captions!")
 
         trainset = GenericImageDataset(path_to_data=path_to_data, 
                                        transform=img_transform, 
-                                       nested=True)
+                                       nested=True,
+                                       return_classes=return_classes)   
+        
+        collate_fn = None
         
     elif dataset == "conceptual_captions":
 
         trainset = conceptual_captions(path_to_data, 
-                                       img_transform, 
-                                       return_caption=return_caption)
+                                       img_transform)
         
+        collate_fn = ConceptualCaptionsCollateFunction(model_name=text_encoder_model, 
+                                                       pre_encoded_text=pre_encoded_text)
     else:
-        
         raise ValueError(f"{dataset} is not Supported")
 
-    return trainset
+    return trainset, collate_fn
 
 if __name__ == "__main__":
 
-    path_to_celeb = "/mnt/datadrive/data/CelebA/img_align_celeba/img_align_celeba/"
     path_to_celebhq = "/mnt/datadrive/data/CelebAMask-HQ/CelebA-HQ-img/"
     path_to_imagenet = "/mnt/datadrive/data/ImageNet/train/"
     path_to_conceptual = "/mnt/datadrive/data/ConceptualCaptions/hf_train"
-    path_to_coco = "/mnt/datadrive/data/coco2017/"
-    path_to_birds = "/mnt/datadrive/data/birds/bird_images/images"
-    path_to_ffhq = "/mnt/datadrive/data/ffhd/images1024x1024"
 
-    loader = get_dataset(dataset="conceptual_captions", 
+    dataset, collate_fn = get_dataset(dataset="conceptual_captions", 
                          path_to_data=path_to_conceptual,
-                         return_caption=True)
+                         pre_encoded_text=True)
     
+    loader = DataLoader(dataset, batch_size=2, collate_fn=collate_fn)
+
     for sample in loader:
         print(sample)
         break
+   
